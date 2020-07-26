3d19e8425b92193b67b63fec4815ba0fac016fbd
nickwang0808 <wang703003515@gmail.com>
2020-07-26 06:07:21 -0400

    Updates

---
+++ precache-manifest.c12eff3827123a9436170ac6f82a90e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cb8ad39d339affe1474a0337f7c99c",
+    "url": "/captain-jump-cut/index.html"
+  },
+  {
+    "revision": "08a9d816c9638e3ff054",
+    "url": "/captain-jump-cut/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d7e34bacfc6e53b338e1",
+    "url": "/captain-jump-cut/static/css/main.0271a63c.chunk.css"
+  },
+  {
+    "revision": "08a9d816c9638e3ff054",
+    "url": "/captain-jump-cut/static/js/2.945e9c88.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/captain-jump-cut/static/js/2.945e9c88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7e34bacfc6e53b338e1",
+    "url": "/captain-jump-cut/static/js/main.2377b42b.chunk.js"
+  },
+  {
+    "revision": "8844a13de8a00c3e9f97",
+    "url": "/captain-jump-cut/static/js/runtime-main.d73fe6ec.js"
+  }
+]);
